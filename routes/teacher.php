327d9da1ce3f5d6072280f83d6971bfa978a268d
ianlabicani <labicaniian@gmail.com>
2025-08-22 14:47:29 +0800

    add route for exam items under teacher prefix

--- routes/teacher.php
+++ routes/teacher.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Teacher\ExamController;
+use App\Http\Controllers\Teacher\ExamItemController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('teacher')->middleware(['auth:sanctum'])->group(function () {
     Route::resource('exams', ExamController::class);
+    Route::resource('exams.items', ExamItemController::class);
 });
