8aaf225a0738f1c4da42d63219c071d303713636
ianlabicani <labicaniian@gmail.com>
2025-08-22 10:30:17 +0800

    use auth sanctum for token issuance

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,20 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = $request->user();
+        $token = $user->createToken($user->name)->plainTextToken;
+        $roles = $user->roles()->pluck('name');
 
-        return response()->noContent();
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+            'roles' => $roles,
+        ]);
+
     }
 
     /**
